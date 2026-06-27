9a730e7fdd0aa3601c3d616e3920eb622536c34b
sizmosloth <samaybagga8@gmail.com>
2026-06-27 11:22:20 +0530

    My Solution of Remove Element

---
+++ Arrays/RemoveElement.cpp
@@ -0,0 +1,18 @@
+// LC NO : 4
+// MY SOLUTION OF Remove Element
+// TOPIC : ARRAYS
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if (nums[i] == val) continue;
+            else{
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
